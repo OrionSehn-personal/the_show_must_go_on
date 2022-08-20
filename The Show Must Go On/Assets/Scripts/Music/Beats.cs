4d55e7e5ce02777e62ac64d2f1bb74d2c4c78287
OrionSehn <orion.sehn@gmail.com>
2022-08-20 14:18:53 -0600

    built math for figuring out good times for beats

--- The Show Must Go On/Assets/Scripts/Music/Beats.cs
+++ The Show Must Go On/Assets/Scripts/Music/Beats.cs
@@ -4,20 +4,40 @@ using UnityEngine;
 
 public class Beats : MonoBehaviour
 {
-    private int speed;
-
+    public float start;
+    public float goodtime1;
+    public float goodtime2;
+    public float speed;
+    public float tolerance;
+    public Vector3 spawnerLocation;
     private KeyCode correctInput;
 
-    public Beats(KeyCode correctInput, int speed)
+    public Beats(
+        KeyCode correctInput,
+        int speed,
+        float tolerance,
+        Vector3 spawnerLocation,
+        Vector3 inputKeyA,
+        Vector3 inputKeyB)
+    // Creates a beat object, takes the location of the spawner, and the two instruments along the lane which it will pass.
     {
-        this.correctInput = correctInput;
+
+        this.start = Time.time;
         this.speed = speed;
+        this.goodtime1 = this.start + ((inputKeyA - spawnerLocation)/speed)
+        this.goodtime2 = this.start + ((inputKeyB - spawnerLocation)/speed)
+
+        this.correctInput = correctInput;
+        this.tolerance = tolerance;
+
     }
 
     // Start is called before the first frame update
     void Start()
     {
+    
         
+
     }
 
     // Update is called once per frame
